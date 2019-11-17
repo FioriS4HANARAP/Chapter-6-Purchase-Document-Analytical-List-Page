0bdc474765021ad7760c1fe2c65d23306c4eaf3e
Fiori S/4HANA RAP <fioris4hanarap@gmail.com>
2019-11-17 13:28:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chapter-6-Purchase-Document-Analytical-List-Page
+This Repository contains the frontend part of the FIORI Elements Analytical List Page (ALP) application developed in Chapter 6 of the SAP Press book **ABAP Development for SAP S/4HANA - ABAP RESTful Programming Model** written by Stefan Haas, Bince Mathew.
+
---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("fiori.display.purchase.documents.PurchaseDocumentsALP.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("fiori.display.purchase.documents.PurchaseDocumentsALP.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
